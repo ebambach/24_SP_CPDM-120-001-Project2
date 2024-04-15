0a4f296fdcc939bee3924167c8f741da8e840c15
ebambach <31412533+ebambach@users.noreply.github.com>
2024-04-15 12:55:39 -0400

    Update ShopClass.py
    Fixed a change from 0 to -1

--- Project2_AQ_EB_KS/Project2_AQ_EB_KS/ShopClass.py
+++ Project2_AQ_EB_KS/Project2_AQ_EB_KS/ShopClass.py
@@ -51,9 +51,9 @@ class ShopClass(object):
                if intInput > -1:
                      self._intInitialSkisInventory = intInput
                else:
-                    raise Exception("Initial Skis Inventory must be an integer equal to or greater than -1. The value of Initial Skis Inventory was: {}".format(intInput))
+                    raise Exception("Initial Skis Inventory must be an integer equal to or greater than 0. The value of Initial Skis Inventory was: {}".format(intInput))
           else:
-               raise Exception("Initial Skis Inventory must be an integer equal to or greater than -1. The value of Initial Skis Inventory was: {}".format(intInput))
+               raise Exception("Initial Skis Inventory must be an integer equal to or greater than 0. The value of Initial Skis Inventory was: {}".format(intInput))
           
     @property
     def intInitialSnowboardsInventory(self):
@@ -65,9 +65,9 @@ class ShopClass(object):
                if intInput > -1:
                      self._intInitialSnowboardsInventory = intInput
                else:
-                    raise Exception("Initial Snowboards Inventory must be an integer equal to or greater than -1. The value of Initial Snowboards Inventory was: {}".format(intInput))
+                    raise Exception("Initial Snowboards Inventory must be an integer equal to or greater than 0. The value of Initial Snowboards Inventory was: {}".format(intInput))
           else:
-               raise Exception("Initial Snowboards Inventory must be an integer equal to or greater than -1. The value of Initial Snowboards Inventory was: {}".format(intInput))
+               raise Exception("Initial Snowboards Inventory must be an integer equal to or greater than 0. The value of Initial Snowboards Inventory was: {}".format(intInput))
 
     
 # ------------------------------------------------------------------
