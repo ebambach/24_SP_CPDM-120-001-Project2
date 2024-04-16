113f32c8c1f6e5bdaac2eb26ebb2abc4cd96b319
ebambach <31412533+ebambach@users.noreply.github.com>
2024-04-16 15:08:46 -0400

    Update Project2_AQ_EB_KS.py
    Added ShopClass tests

--- Project2_AQ_EB_KS/Project2_AQ_EB_KS/Project2_AQ_EB_KS.py
+++ Project2_AQ_EB_KS/Project2_AQ_EB_KS/Project2_AQ_EB_KS.py
@@ -3,15 +3,24 @@
 # Name:                 Team B
 # -----------------------------------------------------------------
 import CustomerClass as Customer
-#import ShopClass as Shop
+import ShopClass as Shop
+
+
 
 # ------------------------------------------------------------------
 # Project 2 Class Test Area
 # ------------------------------------------------------------------
 
 try:
-	test0 = Customer.CustomerClass("Test Case", 1, 1, 2, 1, 0, 0, "")
-	print(repr(test0))
+	# Testing Area
+	test1 = Shop.ShopClass(100,100)  
+
+	test1.calc_estimatebestrentalprice(8, "Hourly", 2, 2)
+	#test0 = Customer.CustomerClass("Crystal Clear", 1, 1, 0, 1, 0, 0, "")
+
+	#print(repr(test0))
+
+	test1.calc_estimatebestrentalprice(10, "Hourly", 1, 0, 0)
 except Exception as e:
 	print(e)
 	
