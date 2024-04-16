78b385d8f1132f9cc5ff20bfbeafe1f30ad07c1c
ksingh2024 <166730794+ksingh2024@users.noreply.github.com>
2024-04-16 16:32:08 -0400

    Update Project2_AQ_EB_KS.py

--- Project2_AQ_EB_KS/Project2_AQ_EB_KS/Project2_AQ_EB_KS.py
+++ Project2_AQ_EB_KS/Project2_AQ_EB_KS/Project2_AQ_EB_KS.py
@@ -26,3 +26,40 @@ except Exception as e:
 	
 	
 input()
+
+
+
+customer1 = strName, intID("Bob", 1)
+customer2 = strName, intID("Lance", 2)
+customer3 = strName, intID("Joe", 3)
+customer4 = strName, intID("Tanner", 4)
+
+# Set up rental basis
+customer1.strRentalBasis == "Hourly"
+customer2.strRentalBasis == "Hourly"
+customer3.strRentalBasis == "Daily"
+customer4.strRentalBasis == "Weekly"
+
+# determine number of skis and snowboards 
+customer1.intSkisRented = 1
+customer2.intSkisRented = 5 (apply_discount)
+customer3.intSnowboardsRented = 2
+customer4.intSnowboardsRented = 0
+
+# detrmine rental time
+customer1.intRentalTime = datetime.now() + timedelta(hours=-4)
+customer2.intRentalTime = datetime.now() + timedelta(hours=-23)
+customer3.intRentalTime = datetime.now() + timedelta(days=-4)
+customer4.intRentalTime = datetime.now() + timedelta(weeks=-14)
+
+# create request to return the skis and snowboards 
+request1 = customer1.returnSkis(intSkisRented)
+request2 = customer2.returnSkis(intSkisRented)
+request3 = customer3.returnSnowBoards(intSnowboardsRented)
+request4 = customer4.returnSnowBoards(intSnowboardsRented)
+
+# return the skis and snowboards and get a bill
+shop1.returnSkis(request1)
+shop1.returnSkis(request2)
+shop1.returnSnowBoards(request3)
+shop1.returnSnowBoards(request4)      
