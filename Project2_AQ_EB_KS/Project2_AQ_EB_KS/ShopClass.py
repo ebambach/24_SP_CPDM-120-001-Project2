172a7dc0c368b9966606a83c610e164e927ad5a7
ebambach <31412533+ebambach@users.noreply.github.com>
2024-04-17 23:14:56 -0400

    Added methods for returned items
    Returned items are added to inventory

--- Project2_AQ_EB_KS/Project2_AQ_EB_KS/ShopClass.py
+++ Project2_AQ_EB_KS/Project2_AQ_EB_KS/ShopClass.py
@@ -245,6 +245,21 @@ class ShopClass(object):
             self.intCurrentSnowboardsInventory -= intRequestedSnowboards
             self._blnValidation = False
 
+	
+# ------------------------------------------------------------------
+# Method to return Skis to inventory
+# ------------------------------------------------------------------
+    def returnSkis(self, intReturnedSkis):
+        self.intCurrentSkisInventory += intReturnedSkis
+
+
+# ------------------------------------------------------------------
+# Method to return Snowboards to inventory
+# ------------------------------------------------------------------
+    def returnSnowboards(self, intReturnedSnowboards):
+        self.intCurrentSnowboardsInventory += intReturnedSnowboards
+
+
 
 # ------------------------------------------------------------------
 # Discount Coupons for Total Items Rented 
